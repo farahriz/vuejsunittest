bd9e7ee0e4f825c7d7e7b04b62a6d1394e7f0be4
farahriz <41633573+farahriz@users.noreply.github.com>
2021-02-02 08:20:53 +0800

    Set up unit test file for AppHeader

---
+++ unit-testing-vue/tests/unit/AppHeader.spec.js
@@ -0,0 +1,7 @@
+import AppHeader from '@/componnts/AppHeader'
+
+describe('AppHeader', () => {
+  test('If user is not logged in, do not show logout button', () => {
+    expect(true).toBe(true)
+  })
+})
