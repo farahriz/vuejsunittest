6212ff9476c7070985bf5fdc78d68b1e9c215885
farahriz <41633573+farahriz@users.noreply.github.com>
2021-02-02 08:25:19 +0800

    Wait for dom to render before asserting test

--- unit-testing-vue/tests/unit/AppHeader.spec.js
+++ unit-testing-vue/tests/unit/AppHeader.spec.js
@@ -9,8 +9,13 @@ describe('AppHeader', () => {
     expect(wrapper.find('button').isVisible()).toBe(false)
   })
 
-  test('If user is logged in, show logout button', () => {
+  test('If user is logged in, show logout button', async () => {
     const wrapper = mount(AppHeader)
+    wrapper.setData({
+      loggedIn: true
+    })
+
+    await wrapper.vm.$nextTick()
     expect(wrapper.find('button').isVisible()).toBe(true)
   })
 
